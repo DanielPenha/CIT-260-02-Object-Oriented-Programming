a8e211c7c6bb702d224c98a026a3d4a519d7d7ff
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-04-27 22:49:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CIT-260-02-Object-Oriented-Programming
+ BYU-I CIT 260 Object Oriented Programming, that uses the textbook - Introduction to Java Programming, Brief Version, 11th Edition ISBN: 9780134671727
---
+++ Week01/src/W01dot1.java
@@ -0,0 +1,13 @@
+public class W01dot1 {
+    public static void main(String[] args) {
+        // write your code here
+        //Displays your name.
+        System.out.println("Daniel Tepedino Penha");
+        //Displays your CIT260 and your section.
+        System.out.println("CIT 260: Object Oriented Programming - Section 02");
+        //Displays your home town.
+        System.out.println("Vila Velha - Espirito Santo - Brazil");
+        //Displays your favorite dessert. Pessegos em calda com creme de Leite
+        System.out.println("Peaches in syrups with sour cream");
+    }
+}
