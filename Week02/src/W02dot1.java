cae70d383f6ded8dc9e5f9c9ae54ae9ecd553147
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-04-29 13:36:24 -0300

    Create and started the files for the second week

---
+++ Week02/src/W02dot1.java
@@ -0,0 +1,19 @@
+/*
+ * W02 Assignment: Program 2.1
+ Write a short Java program that does the following:
+
+ Tells the user what the program does.
+ Prompts the user to enter a temperature in degrees Celsius.
+ Gets the user's input and saves it in a variable declared as a double.
+ Converts the Celsius temperature to a temperature in degrees Fahrenheit, using the formula tempf = (9/5) tempc+ 32
+ Outputs the Celsius temperature and the Fahrenheit temperature, with 2 digits after the decimal point.
+ Outputs a goodbye message.
+  CIT 260 - 02
+  Autor@ Daniel Penha* /
+*/
+
+public class W02dot1 {
+    public static void main(String[] args){
+        System.out.println("Daniel!");
+    }
+}
---
+++ Week02/src/W02dot2.java
@@ -0,0 +1,21 @@
+/*
+ * W02 Assignment: Program 2.2
+ Write a short Java program that does the following:
+
+ Tells the user what the program does.
+ Prompts the user to enter a value for the cost of their meal.
+ Gets the user's input and saves it in a variable declared as a double.
+ Prompts the user to enter a percentage value for the tip.
+ Gets the user's input and saves it in a variable declared as a double.
+ Calculates the value of the tip, using the percentage entered by the user.
+ Calculates the tax on the meal. The tax rate is 3.2%.
+ Calculates the total bill = meal cost + tip + tax.
+ Outputs the tip, the tax, and the total value of the bill.
+       All values will be formatted to show a dollar sign and 2 digits after the decimal point.
+ Outputs a goodbye message.
+  CIT 260 - 02
+   Autor@ Daniel Penha
+*/
+
+public class W02dot2 {
+}
