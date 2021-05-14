f0c0004c59a2e8d3f21afd50396d829678a5a604
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-05-14 07:05:55 -0300

    Week04
    Start the file for week04

---
+++ Week04/src/W04dot1.java
@@ -0,0 +1,32 @@
+/*W04 Assignment: Program 4.1
+Write a short Java program that does the following:
+
+Tells the user what the program does.
+Prompts the user to enter a single hexadecimal digit.
+Gets the user's input and saves it. If the value is not a valid hexadecimal digit tell the user and terminate the program.
+Converts the hexadecimal digit into a four digit binary number. You may not use any of the conversion methods built into Java, such as toBinaryString( ), to do this.
+Outputs the four digit binary number, including any leading zeros.
+Outputs a goodbye message.
+
+***********
+Here are some examples of what your program should look like when it runs (user input is in green):
+***********
+   This program converts a hexadecimal digit into a four digit binary number.
+   Enter a hexadecimal number: B
+
+   The binary value is 1011.
+   Goodbye.
+------------
+ This program converts a hexadecimal digit into a four digit binary number.
+   Enter a hexadecimal number: G
+
+   G is not a valid hexadecimal digit.
+   Goodbye.
+-------------
+CIT 260 - 02
+Author@ Daniel Penha
+
+*/
+
+public class W04dot1 {
+}
---
+++ Week04/src/W04dot2.java
@@ -0,0 +1,40 @@
+/*W04 Assignment: Program 4.2
+Write a short Java program that does the following:
+
+Tells the user what the program does.
+Prompts the user to enter their first and last name.
+Gets the user's input and saves it as a String.
+Prompts the user to enter the number of hours they worked this week.
+Gets the user's input and saves it.
+Prompts the user to enter their hourly wage.
+Get's the user's input and saves it.
+Calculates the state withholding tax (9%) and the federal withholding tax (20%).
+Calculates the gross pay and the net pay after subtracting the withholding taxes.
+Outputs a pay statement as shown in the example below.
+Outputs a goodbye message.
+************
+Here is an example of what your program should look like when it runs (user input is in green):
+------------
+   Given your name, hours worked, and hourly wage, this program calculates your
+   gross pay, state withholding tax, federal withholding tax, and your net pay.
+   It then displays your pay stub.
+   Enter your first and last name: John Brown
+   Enter the hours you worked this week: 10.0
+   Enter your hourly wage: 9.75
+
+   Pay Stub for John Brown
+   Hours Worked: 10
+   Hourly Wage: $9.75
+   Gross Pay: $97.50
+   State Tax Withheld: $8.78
+   Federal Tax Withheld: $19.50
+   Net Pay: $69.23
+
+   Goodbye.
+---------------
+CIT 260 - 02
+Author@ Daniel Penha
+*/
+
+public class W04dot2 {
+}
