68672a983ae974668807d7009d9ca099ea76b5f1
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-05-20 15:24:47 -0300

    Week05 assignments

---
+++ Week05/src/W05dot1.java
@@ -0,0 +1,23 @@
+/*W05 Assignment: Program 5.1
+Write a short Java program that does the following:
+
+Tells the user what the program does.
+Uses a loop to calculate pounds per kilogram (1 kilogram = 2.2 pounds) and display the following table.
+Outputs a goodbye message.
+        kilograms    pounds
+        ---------    ------
+         1           2.2
+         3           6.6
+         5          11.0
+         7          15.4
+         9          19.8
+        11          24.2
+        13          28.6
+        15          33.0
+----------------------------------------------------------------
+CIT 260 - 02
+Author@ Daniel Penha
+        */
+
+public class W05dot1 {
+}
---
+++ Week05/src/W05dot2.java
@@ -0,0 +1,46 @@
+/*W05 Assignment: Program 5.2
+Write a short Java program that does the following:
+
+Tells the user what the program does.
+Uses a loop to display a table of all of the numbers from 100 to 1000 that are divisible by both 5 and 6.
+   The numbers must be separated by exactly one space and there must be 10 numbers per line.
+Displays a goodbye message.
+Your output should look like the following:
+
+This program displays all of the numbers from 100 to 1000
+that are divisible by both 5 and 6.
+
+120 150 180 210 240 270 300 330 360 390
+420 450 480 510 540 570 600 630 660 690
+720 750 780 810 840 870 900 930 960 990
+
+Goodbye
+
+----------------------------------------------------------------
+CIT 260 - 02
+Author@ Daniel Penha
+*/
+
+public class W05dot2 {
+    public static void main(String[] args) {
+        // 1)  Tells the user what the program does.
+        System.out.println("This program displays all of the numbers from 100 to 1000\n" +
+                "that are divisible by both 5 and 6. ");
+        // 2) Uses a loop to display a table of all of the numbers from 100 to 1000 that are divisible by both 5 and 6.
+        int elements = 1;
+        for (int i = 100 ; i <= 1000 ; i++){
+            if (i % 5 == 0 && i % 6 == 0){
+                if (elements%10!=0) {
+                    System.out.print(i+" ");
+                    elements++;
+                } else {
+                    System.out.println(i);
+                    elements = 1;
+                }
+                //System.out.println(i);
+                //System.out.printf("%d %d %d %d %d %d %d %d %d %d\n" , i );
+            }
+        }
+        System.out.println("Goodbye");
+    }
+}
