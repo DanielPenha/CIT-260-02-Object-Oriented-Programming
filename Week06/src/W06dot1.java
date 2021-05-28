84c8b8b99fb87df900bf964997a3bafc1d5e10e7
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-05-28 17:47:02 -0300

    Assignment Week06 completed!

--- Week06/src/W06dot1.java
+++ Week06/src/W06dot1.java
@@ -1,3 +1,5 @@
+import java.util.Currency;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**W06 Assignment: Program 6.1
@@ -44,45 +46,65 @@ import java.util.Scanner;
  * */
 public class W06dot1 {
     public static void main(String[] args) {
+
+        Currency correntLocate = Currency.getInstance(Locale.getDefault());
         //1) Tells the user what the program does.
         System.out.println("Given an investment amount and an annual interest rate, this program \n" +
                 "will calculate the future value of the investment for a period of\n" +
                 "ten years.\n");
 
         //2) Prompts the user to an investment amount, for example, 1000.
-        System.out.println("Enter the a positive, non-zero value for the investment:");
-
-        //3) Gets the user's input and saves it. If the user's input is not a positive, non-zero value, display an error message, then loop back and prompt for a new value. Stay in this loop until you have a valid value.
         Scanner input = new Scanner(System.in);
-        int investmentAmount = input.nextInt();
 
-        boolean positiveInterest = false;
+        boolean positiveValue = false;
+        int investmentAmount = 0;
 
-        if (investmentAmount >= 0 && investmentAmount != < 0)
-            positiveInterest = true;
+        while (positiveValue == false) {
+
+            //3) Gets the user's input and saves it. If the user's input is not a positive, non-zero value, display an error message, then loop back and prompt for a new value. Stay in this loop until you have a valid value.
+            System.out.println("Enter the a positive, non-zero value for the investment:");
+
+
+            investmentAmount = input.nextInt();
+
+            if (investmentAmount > 0)
+                positiveValue = true;
+        }
 
         //4) Prompts the user to enter an annual interest rate between 0 and 100. For example, 9 would be 9%.
-        System.out.println("Enter an annual interest rate, between 0 and 100:");
-        else{
-                System.out.println("Interest amount must be positive, non-zero number between 1 and 100.");
-                System.exit();
+
+        positiveValue = false;
+        double annualInterestRate = 0;
+        //5) Gets the user's input and saves it. If the user's input is not positive or if it is greater than 100, display an error message, then loop back and prompt for a new value. Stay in this loop until you have a valid value.
+
+        while (positiveValue == false) {
+
+            if (annualInterestRate > 0 && annualInterestRate <= 100) {
+                positiveValue = true;
+            } else {
+                System.out.println("Please, enter an annual interest rate, between 0 and 100:");
+                annualInterestRate = input.nextDouble();
             }
 
+        }
+        System.out.printf("%6s     %s\n" , "Years" , "Future Value");
 
-
-        //5) Gets the user's input and saves it. If the user's input is not positive or if it is greater than 100, display an error message, then loop back and prompt for a new value. Stay in this loop until you have a valid value.
-        int annualInterestRate = input.nextInt();
-
-        //6) Using a method that you have written, calculate and display the future value of the investment
-        //  for a period of 10 years. The formula for computing the future value of an investment is
-        //     futureValue = investmentAmount x (1 + monthlyInterestRate)numberOfYears * 12
-        //**  The method header for your method should look like this:
-        //public static double futureValue(double investmentAmount, double monthlyInterestRate, int years)*/
-
-
-        System.out.printf("%s %s\n" , "Years" , "Future Value");
+        for (int year = 1; year <=10 ; year++) {
+            double futureValue = futureValue(investmentAmount, annualInterestRate, year);
+            System.out.printf("%6s     "+correntLocate.getSymbol()+"%.2f\n" , year , futureValue);
+        }
 
         //Say goodbye
         System.out.println("\nGoodbye ....");
+    }
+    //6) Using a method that you have written, calculate and display the future value of the investment
+    //  for a period of 10 years. The formula for computing the future value of an investment is
+    //     futureValue = investmentAmount x (1 + monthlyInterestRate)numberOfYears * 12
+    //**  The method header for your method should look like this:
+    //public static double futureValue(double investmentAmount, double monthlyInterestRate, int years)*/
+    public static double futureValue(double investmentAmount, double annualInterestRate, int years){
+        double futureValue = investmentAmount * Math.pow((1 + annualInterestRate/100.0), years);
+
+        return futureValue;
     }
 }
