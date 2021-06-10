2d0197fd111325fa0afa83cfd196b950888dc456
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-06-10 00:45:50 -0300

    Week08 assignments files created.

--- Week08/src/W08dot1.java
+++ Week08/src/W08dot1.java
@@ -1,4 +1,37 @@
-/**
+/**W08 Assignment: Program 8.1
+ Using the example of the Circle class from section 9.2 of the textbook design a Rectangle class. Create a UML class diagram that shows your design. Your UML class diagram must be submitted as part of this assignment. The preferred format for you class diagram is a .PDF file. This week's Powerpoint slides describe how to create a UML class digram. Your Rectangle class should contain the following:
+
+ Two data fields named width and height, that specify the width and height of the rectangle. The default values for both width and height should be 1.
+ A no-arg constructor that creates a default rectangle object.
+ A parameterized constructor that creates a rectangle object with the specified height and width.
+ Getter and setter methods for height and width.
+ A method named getArea( ) that computes and returns the area of the rectangle.
+ A method names getPerimeter( ) that computes and returns the perimeter of the rectangle.
+ Your file should have the proper file prologue (comments) and each method should have the proper method prologue (comments).
+ Then write a short Java program that implements your Rectangle class, and a main() method that does the following:
+
+ Tells the user what the program does.
+ Creates two Rectangle objects, the first with a height of 4 and a width of 40, the second with a height of 3.5 and a width of 5.
+ Call the methods in your Rectangle class to output the width, height, area, and perimeter of each rectangle, with 2 digits after the decimal point.
+ Output a goodbye message.
+ The following example illustrates what your program should look like when it runs.
+
+ This program creates two rectangle objects and displays their width, height, area and perimeter.
+
+ Rectangle 1:
+ height = 4.00
+ width = 40.00
+ area = 160.00
+ perimeter = 88.00
+
+ Rectangle 2:
+ height = 3.50
+ width = 5.00
+ area = 17.50
+ perimeter = 17.00
+
+ goodbye ...
+
  * CIT 260 - 02
  * Author@ Daniel Penha*/
 
--- Week08/src/W08dot2.java
+++ Week08/src/W08dot2.java
@@ -1,4 +1,33 @@
-/**
+/**W08 Assignment: Program 8.2
+ Design and create a class that represents a bank account. Name the class BankAccount. Create a UML class diagram that shows your design. Your UML class diagram must be submitted as part of this assignment. Your Bank class should contain the following:
+
+ A data field named accountNumber that holds the account number for the account. This field is an integer.
+ A data field named balance that holds the balance in the account. This field is a double.
+ A no-arg constructor that creates a default account object. It sets both data fields to zero.
+ A parameterized constructor that creates an account object with the specified account number and balance.
+ Getter methods for accountNumber and balance.
+ A method named makeDeposit( ) that takes a double as its argument, and adds the value of the argument to the balance.
+ A method names makeWithdrawal( ) that takes a double as its argument, and subtracts the value of the argument from the balance.
+ Then write a short Java program that implements your BankAccount class, and a main() method that does the following:
+
+ Tells the user what the program does. This program has no user input.
+ Creates a BankAccount object with an account number of 123 and an initial balance of $5.00
+ Makes a deposit of $10.50 into this account.
+ Makes a deposit of $3.25 into this account.
+ Makes a withdrawal of $1.50 from this account.
+ Displays the current balance in the account as shown in the example below.
+ Output a goodbye message.
+ The following example illustrates what your program should look like when it runs. Your code should be well documented with appropriate file prologues, method prologues and comments.
+
+ This program tests the BankAccount class by ...
+ ...Creating account number 123 with an initial balance of $5.00
+ ...Making a deposit of $10.50
+ ...Making a deposit of $3.25
+ ...Making a withdrawal of $1.50
+ The balance in account number 123 is $17.25
+ Goodbye...
+
+
  * CIT 260 - 02
  * Author@ Daniel Penha*/
 
