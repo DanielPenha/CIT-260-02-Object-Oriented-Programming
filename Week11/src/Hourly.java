c74962c0fcbde0e0c9061b5ec52234f52338e36c
Daniel Tepedino Penha <danielpenha@yahoo.com.br>
2021-07-02 14:27:59 -0300

    W11dot1 Adjustments on the classes.

--- Week11/src/Hourly.java
+++ Week11/src/Hourly.java
@@ -1,5 +1,41 @@
-public class Hourly {
-    public static void main(String[] args) {
-        System.out.println("Hourly");
+public class Hourly extends Employee {
+    //Attributes
+    //1) A double, hourlyWage,  that holds this person's hourly wage.
+    private double hourlyWage;
+
+    //2) A double, hoursWorked, that holds the number of hours this person worked.
+    private double hoursWorked;
+
+    //Constructor
+
+    //3) An appropriate no-arg constructor.
+    public Hourly(){
+
     }
+
+    //4) An appropriate parameterized constructor.
+    public Hourly ( double hourlyWage , double hoursWorked ){
+        this.hourlyWage = hourlyWage;
+        this.hoursWorked = hoursWorked;
+    }
+    
+    //GETTERS & SETTERS
+    //5) Getters for hourlyWage and hoursWorked.
+    //hourlyWage
+    public double getHourlyWage() {
+        return hourlyWage;
+    }
+
+    //hoursWorked
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    //Methods
+    //6) Override the getGrossPay( ), getFedWithholding( ) and getStateWithholding( )
+    // methods from the Employee class.
+    // The gross pay for an Hourly employee is calculated as hoursWorked x hourlyWage.
+    // Use a state tax rate of 0.07 and a federal tax rate of 0.15.
+    /*@Override
+    public */
 }
--- Week11/src/Salaried.java
+++ Week11/src/Salaried.java
@@ -1,5 +1,34 @@
-public class Salaried {
-    public static void main(String[] args) {
-        System.out.println("Salaried");
+public class Salaried extends Employee{
+    //Attributes
+    //1) A double, annualSalary, that holds the annual salary for this person.
+    private double annualSalary;
+
+    //Constructor
+    //2) An appropriate no-arg constructor.
+    public Salaried(){
+
     }
+
+    //3) An appropriate parameterized constructor.
+    public Salaried( double annualSalary){
+        this.annualSalary = annualSalary;
+    }
+
+    //GETTERS & SETTERS
+    //4) A Getter for salary.
+    public double getAnnualSalary() {
+        return annualSalary;
+    }
+
+    //Methods
+    //5) Override the getGrossPay( ), getFedWithholding( ) and getStateWithholding( ) methods
+    // from the Employee class.
+    // The gross pay for a Salaried employee is calculated as annualSalary / 52.
+    // Use a state tax rate of 0.07 and a federal tax rate of 0.15.
+    /*@Override
+    public */
+
+
+
+
 }
